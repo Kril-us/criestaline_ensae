a9504c79a6c22ceb513b518ee811fbe956cba734
Kril-us <cyril.houdaille@gmail.com>
2025-02-26 19:10:55 +0100

    caca

--- projet_python_ensae_2025/ford_fulkerson.py
+++ projet_python_ensae_2025/ford_fulkerson.py
@@ -8,6 +8,8 @@ from collections import defaultdict
 #     fonction de hachage (dict en python) qui à chaque arc d'un graphe associe
 #     une quantité correspondante.
 
+def caca():
+    return "prout"
 
 def construire_réseau(liste):
     """Aide à construire la structure de graphe orienté et de capacité.
